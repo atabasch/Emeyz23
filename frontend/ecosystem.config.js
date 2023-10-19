b99cd48414c4aa3bd6672a10c938ef2a35fd5120
Furkan A <asw13tr@gmail.com>
2023-10-19 16:37:57 +0000

    updated

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -5,7 +5,8 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start'
+      args: 'start',
+	error_file: '/var/www/Emeyz23/error.log'
     }
   ]
 }
